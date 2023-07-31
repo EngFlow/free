8ee81b8be78eb46d66f884b5a21079b43e2f121e
zaitrarrio <zaitrarrio@gmail.com>
2023-07-30 23:34:24 -0500

    chore: add clean to ensure remote execution for testing and validation

--- example/entrypoint.sh
+++ example/entrypoint.sh
@@ -5,7 +5,6 @@ docker login -u=engflow -p=engflow localhost:5443
 docker build ./worker -t localhost:5443/engflow/worker:latest
 docker push localhost:5443/engflow/worker
 
-# echo curl -v -u engflow:engflow https://host.docker.internal:5043/v2/
-
 pushd /home/engflow/example || exit 1
+bazel clean
 bazel --bazelrc=engflow.bazelrc build //cpp/... --config=engflow
